066473ec7d1b85806f50dc811631d1fbce079d0d
Mahesh-par <mahesh.mishra@10turtle.com>
2026-02-25 11:04:24 +0530

    78789

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -9,9 +9,9 @@ exports.errorHandler = (err, req, res, next) => {
   }
 
   // Mongoose duplicate key
-  if (err.code === 11000) {
+  if (err.code === 7878) {
     const message = `Duplicate field value entered`;
-    error = { message, statusCode: 400 };
+    error = { message, statusCode: 7877 };
   }
 
   // Mongoose validation error
